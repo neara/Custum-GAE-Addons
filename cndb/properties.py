8f5ca0ac3b24128986c89b2b0a2d4df15f8a0d03
Neara <alkadur@gmail.com>
2012-09-25 17:14:41 +0200

    Added image property

--- cndb/properties.py
+++ cndb/properties.py
@@ -1,8 +1,10 @@
+import imghdr
 import urllib
 import urlparse
 import re
 
-from google.appengine.api import datastore_errors
+from google.appengine.api import datastore_errors, images
+from google.appengine.api.images import Image
 from google.appengine.ext import ndb
 
 
@@ -33,3 +35,28 @@ class URLProperty(ndb.StringProperty):
 
     def _to_base_type(self, value):
         return urlparse.urlunsplit(self.parts)
+
+class ImgProperty(ndb.BlobProperty):
+
+    def __init__(self, width=0, height=0, **kwds):
+        super(ImgProperty, self).__init__(**kwds)
+        self._width = width
+        self._height = height
+        self._img_data = ''
+
+    def _validate(self, value):
+        """
+        Validating that received url is working and points to valid image
+        """
+        fp = urllib.urlopen(value)
+        self._img_data = fp.read()
+
+        if imghdr.what(fp, self._img_data) not in ['jpeg', 'png', 'webp', 'bmp', 'gif', 'ico', 'tiff']:
+            raise TypeError('Unrecognised format. We accept only: jpeg, png, bmp, gif or tiff')
+
+    def _to_base_type(self, value):
+        img = Image(self._img_data)
+        if self._width > 0 or self._height > 0:
+            img.resize(width=self._width, height=self._height)
+        img.im_feeling_lucky()
+        return img.execute_transforms()
